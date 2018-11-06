4f21eb88056dc2f89623626b950f59a55164eb13
Pandaoil <40745595+JKinoial15@users.noreply.github.com>
2018-11-06 11:24:57 +1100

    Enemy spawn unfinished

---
+++ Contra Clone/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    public GameObject enemy;
+    public GameObject location;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("Spawner", 1.0f, 3.0f);
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Spawner()
+    {
+        print(Time.time);
+        Instantiate(enemy,location.transform);
+        
+       
+     
+    }
+
+}
