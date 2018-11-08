f0d4a6301ff2b67dc1d3c4c873b0460da5172c73
StephMcleod <40745594+StephMcleod@users.noreply.github.com>
2018-11-08 17:43:00 +1100

    Created MainMenuController script
    Created Testing scenes, and Script for the menu controller.

---
+++ Contra Clone/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour {
+
+
+		//loadscene
+	public void PlayGame()
+	{
+		SceneManager.LoadScene ("Stephen_");
+	}
+		//exit
+	public void Quit()
+	{
+		Application.Quit();
+	}
+	public void Credits()
+	{
+		SceneManager.LoadScene("Credits_SW");
+	}
+	public void OpenOptions(){}
+}	
