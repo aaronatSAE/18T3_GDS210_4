2ec9be8d3167ded1568b53efe5484eb4a9d8a1ff
JKinoial <40745595+JKinoial15@users.noreply.github.com>
2018-11-12 02:00:56 +1100

    machine gun and level layout
    the level layout is basic as i dont have player jumping and player size.
    the machine is working fine i just need to make it damaging enemies.

---
+++ Contra Clone/Assets/Scripts/MachineGun.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MachineGun : MonoBehaviour 
+{
+	
+	//Variables
+	public float magnitude = 1.0f;
+	public float lifeLine = 1.0f;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		//Using the Rigidbody component it adds force in the forward direction
+		GetComponent<Rigidbody> ().AddForce (magnitude * transform.forward);
+
+		//Destroys the bullet when the lifeline is up
+		Destroy (gameObject, lifeLine);
+
+
+	}
+
+
+	 //When the object collides with anything it'll destroy itself
+	void OnCollisionEnter(Collision collider) 
+	{
+		//Destroys the gameobject
+		Destroy (gameObject);
+	}
+}
---
+++ Contra Clone/Assets/Scripts/Shooting.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour 
+{
+	//Variables
+	public GameObject bullet;
+	public float delayDuration = 1.0f;
+	private float timeOfShot = 0.0f;
+
+
+	void Start()
+	{
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        //When the player clicks the left mouse button the gun will fire
+        if (Input.GetButton("Fire1"))
+        {
+            // if statement used so that there is a delay between shots
+            if (Time.time > timeOfShot + delayDuration)
+            {
+                //Creates a bullet
+                Instantiate(bullet, transform.position, transform.rotation);
+
+                //makes the time when shot equal to time in game
+                timeOfShot = Time.time;
+            }
+
+        }
+    }
+}
