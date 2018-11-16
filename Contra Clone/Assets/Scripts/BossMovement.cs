92ef46c9e6fa77b0407335a4bf23806dee755c2f
JKinoial <40745595+JKinoial15@users.noreply.github.com>
2018-11-16 18:44:40 +1100

    Boss (inprogress)

---
+++ Contra Clone/Assets/Scripts/BossMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossMovement : MonoBehaviour {
+
+    public GameObject 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Movement()
+    {
+
+    }
+
+    public void Attack1()
+    {
+
+    }
+
+    public void Attack2()
+    {
+
+    }
+
+    public void Attack3()
+    {
+
+    }
+}
