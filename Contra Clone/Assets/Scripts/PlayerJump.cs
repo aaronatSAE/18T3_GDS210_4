8306e6a7229c562c0dbe83621b526b9ac19df3d0
Gords1229 <40745592+Gords1229@users.noreply.github.com>
2018-11-16 15:55:54 +1100

    movement and jump scripts done
    can tweek numbers later to make it feel good, climbing and slide to come next

---
+++ Contra Clone/Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour {
+
+    
+    public Rigidbody playerRB;
+    private Vector3 playerVelocity;    
+    public float jumpSpeed = 1.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        playerRB = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetButton("Jump") == true)
+        {            
+            playerVelocity = playerRB.velocity;
+            playerVelocity.y = jumpSpeed;
+            playerRB.velocity = playerVelocity;
+        }
+
+        //Debug.Log(Input.GetButtonDown("Jump") + ": " + playerRB.velocity);
+    }   
+}
+  
---
+++ Contra Clone/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody playerRB;
+    private Vector3 playerVelocity;
+    public float runSpeed = 1.0f;
+    public float airSpeed = 1.0f;
+    public float climbSpeed = 1.0f;
+    private float deltaX;
+    private bool grounded = false;
+    private bool onBar = false;
+    
+
+	// Use this for initialization
+	void Start ()
+    {
+        playerRB = GetComponent<Rigidbody>();        
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {        
+        playerVelocity = playerRB.velocity;
+
+        if (Input.GetAxis("Horizontal") != 0.0f)
+        {
+            if(grounded == true)
+            {
+                deltaX = runSpeed * Input.GetAxis("Horizontal") / Mathf.Abs(Input.GetAxis("Horizontal"));
+            }
+            else if(onBar == true)
+            {
+                deltaX = climbSpeed * Input.GetAxis("Horizontal") / Mathf.Abs(Input.GetAxis("Horizontal"));
+            }
+            else
+            {
+                deltaX = airSpeed * Input.GetAxis("Horizontal") / Mathf.Abs(Input.GetAxis("Horizontal"));
+            }
+            
+            playerVelocity.x = deltaX;
+            playerRB.velocity = playerVelocity;            
+        }
+        else
+        {
+            //player will quickly come to a stop when grounded or on a monkey bar and not hitting a direction
+            //in the air players will maintain their momentum when not hitting a direction
+            if (grounded == true || onBar == true)
+            {
+                playerVelocity.x = 0;
+                playerRB.velocity = playerVelocity;
+            }           
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Platform")
+        {
+            grounded = true;
+        }
+        else if(other.gameObject.tag == "Bar")
+        {
+            onBar = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.tag == "Platform")
+        {
+            grounded = false;
+        }
+        else if (other.gameObject.tag == "Bar")
+        {
+            onBar = false;
+        }
+    }
+}
+
