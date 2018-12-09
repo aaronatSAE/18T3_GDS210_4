69b22892bcbe6a81d1303231b042a41c40d12a38
StephMcleod <40745594+StephMcleod@users.noreply.github.com>
2018-12-10 01:27:20 +1100

    Added Audio files and Dialogue trigger script
    Dialogue Trigger script doesn't seem to be working, it could be the test scene however.

---
+++ Contra Clone/Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueTrigger : MonoBehaviour {
+
+	public GameObject dBox;
+	public GameObject dTrigger;
+	
+	public void OnTriggerEnter(Collider  col)
+	{
+		//print("hello");
+		
+		if(col.tag == "Player")
+		{
+			Debug.Log("yes");
+			DisplayD();
+		}
+	}
+	void DisplayD()
+	{			
+		dBox.SetActive (true);
+		Destroy(dTrigger);
+	}
+}
