b7810dffda41c43e354f89269bfcbab9dbf51c0f
StephMcleod <40745594+StephMcleod@users.noreply.github.com>
2018-12-10 14:33:23 +1100

    Dialogue trigger / cutscene killer

---
+++ Contra Clone/Assets/Scripts/DialogueDisable.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueDisable : MonoBehaviour {
+
+	public float duration;
+	//Destroy Cutscenes
+	void Start()
+	{
+		Destroy (gameObject, duration);
+	}
+}
