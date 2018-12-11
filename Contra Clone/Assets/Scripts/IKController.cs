d29d85953763311e01c767fb042f21759d4add15
Gords <40745592+Gords1229@users.noreply.github.com>
2018-12-11 15:59:03 +1100

    a little progress to IK animation

---
+++ Contra Clone/Assets/Scripts/IKController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IKController : MonoBehaviour {
+
+    protected Animator animator;
+
+    public bool barShooting = false;
+    public bool sliding = false;
+
+    public Transform rightHandOnGun;
+    public Transform leftHandOnGun;
+    public Transform rightHandHanging;
+    public Transform leftHandHanging;
+    
+    
+    // Use this for initialization
+	void Start ()
+    {
+        animator = GetComponent<Animator>();
+	}
+
+    //a callback for calculating IK
+    private void OnAnimatorIK()
+    {
+        if (animator)
+        {
+            if (barShooting)
+            {
+                animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
+                animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 1);
+                animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1);
+                animator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1);
+
+                animator.SetIKPosition(AvatarIKGoal.RightHand, rightHandHanging.position);
+                animator.SetIKRotation(AvatarIKGoal.RightHand, rightHandHanging.rotation);
+                animator.SetIKPosition(AvatarIKGoal.LeftHand, leftHandHanging.position);
+                animator.SetIKRotation(AvatarIKGoal.LeftHand, leftHandHanging.rotation);
+            }
+
+            //Debug.Log("test");
+        }
+    }
+}
--- Contra Clone/Assets/Scripts/SniperEnemy.cs
+++ Contra Clone/Assets/Scripts/SniperEnemy.cs
@@ -1,52 +1,52 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class SniperEnemy : MonoBehaviour {
+//public class SniperEnemy : MonoBehaviour {
 
     
-    public GameObject playerLocation;
-    [SerializeField] private GameObject gun;
-    public GameObject bullet;
-    public float delayDuration = 1.0f;
-    private float timeOfShot = 0.0f;
+//    public GameObject playerLocation;
+//    [SerializeField] private GameObject gun;
+//    public GameObject bullet;
+//    public float delayDuration = 1.0f;
+//    private float timeOfShot = 0.0f;
 
 
-    // Use this for initialization
-    void Start () {
-        StartCoroutine("CoLocate");
+//    // Use this for initialization
+//    void Start () {
+//        StartCoroutine("CoLocate");
 
-    }
+//    }
 	
-	// Update is called once per frame
-	void Update () {
-        if (playerLocation.transform.position.x > transform.position.x)
-        {
-            transform.localRotation = Quaternion.Euler(0, 90, 0);
-        }
-        else
-        {
-            transform.localRotation = Quaternion.Euler(0, -90, 0);
-        }
-    }
+//	// Update is called once per frame
+//	void Update () {
+//        if (playerLocation.transform.position.x > transform.position.x)
+//        {
+//            transform.localRotation = Quaternion.Euler(0, 90, 0);
+//        }
+//        else
+//        {
+//            transform.localRotation = Quaternion.Euler(0, -90, 0);
+//        }
+//    }
 
-    public IEnumerator CoLocate()
-    {
-        while(gameObject != null)
-        {
+//    public IEnumerator CoLocate()
+//    {
+//        while(gameObject != null)
+//        {
 
-            gun.transform.LookAt(playerLocation.transform);
-            // if statement used so that there is a delay between shots
-            if (Time.time > timeOfShot + delayDuration)
-            {
-                //Creates a bullet
-                Instantiate(bullet, gun.transform.position, gun.transform.rotation);
+//            gun.transform.LookAt(playerLocation.transform);
+//            // if statement used so that there is a delay between shots
+//            if (Time.time > timeOfShot + delayDuration)
+//            {
+//                //Creates a bullet
+//                Instantiate(bullet, gun.transform.position, gun.transform.rotation);
 
-                //makes the time when shot equal to time in game
-                timeOfShot = Time.time;
-            }
-            yield return new WaitForSeconds(1f);
-        }
+//                //makes the time when shot equal to time in game
+//                timeOfShot = Time.time;
+//            }
+//            yield return new WaitForSeconds(1f);
+//        }
         
-    }
-}
+//    }
+//}
