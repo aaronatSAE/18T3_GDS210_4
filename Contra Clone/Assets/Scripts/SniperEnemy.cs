b5429b187492c2eab791f218997e66ae6efae78c
JKinoial <40745595+JKinoial15@users.noreply.github.com>
2018-12-11 18:52:25 +1100

    Sniper done

--- Contra Clone/Assets/Scripts/SniperEnemy.cs
+++ Contra Clone/Assets/Scripts/SniperEnemy.cs
@@ -1,52 +1,57 @@
-﻿//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-//public class SniperEnemy : MonoBehaviour {
-
-    
-//    public GameObject playerLocation;
-//    [SerializeField] private GameObject gun;
-//    public GameObject bullet;
-//    public float delayDuration = 1.0f;
-//    private float timeOfShot = 0.0f;
+public class SniperEnemy : MonoBehaviour
+{
 
 
-//    // Use this for initialization
-//    void Start () {
-//        StartCoroutine("CoLocate");
+    public GameObject playerLocation;
+    [SerializeField] private GameObject gun;
+    public GameObject bullet;
+    public float delayDuration = 1.0f;
+    private float timeOfShot = 0.0f;
 
-//    }
-	
-//	// Update is called once per frame
-//	void Update () {
-//        if (playerLocation.transform.position.x > transform.position.x)
-//        {
-//            transform.localRotation = Quaternion.Euler(0, 90, 0);
-//        }
-//        else
-//        {
-//            transform.localRotation = Quaternion.Euler(0, -90, 0);
-//        }
-//    }
 
-//    public IEnumerator CoLocate()
-//    {
-//        while(gameObject != null)
-//        {
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine("CoLocate");
 
-//            gun.transform.LookAt(playerLocation.transform);
-//            // if statement used so that there is a delay between shots
-//            if (Time.time > timeOfShot + delayDuration)
-//            {
-//                //Creates a bullet
-//                Instantiate(bullet, gun.transform.position, gun.transform.rotation);
+    }
 
-//                //makes the time when shot equal to time in game
-//                timeOfShot = Time.time;
-//            }
-//            yield return new WaitForSeconds(1f);
-//        }
-        
-//    }
-//}
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerLocation.transform.position.x > transform.position.x)
+        {
+            transform.localRotation = Quaternion.Euler(0, 90, 0);
+        }
+        else
+        {
+            transform.localRotation = Quaternion.Euler(0, -90, 0);
+        }
+    }
+
+    public IEnumerator CoLocate()
+    {
+        while (gameObject != null)
+        {
+
+            gun.transform.LookAt(playerLocation.transform);
+            // if statement used so that there is a delay between shots
+            if (Time.time > timeOfShot + delayDuration)
+            {
+                //Creates a bullet
+                Instantiate(bullet, gun.transform.position, gun.transform.rotation);
+
+                Debug.Log("Bang");
+
+                //makes the time when shot equal to time in game
+                timeOfShot = Time.time;
+            }
+            yield return new WaitForSeconds(1f);
+        }
+
+    }
+}
