11fecba2ee568b604239561456132a666d7b8298
JKinoial <40745595+JKinoial15@users.noreply.github.com>
2018-12-14 14:56:40 +1100

    Explosions added

--- Contra Clone/Assets/Scripts/EnemyHealth.cs
+++ Contra Clone/Assets/Scripts/EnemyHealth.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class EnemyHealth : MonoBehaviour {
 
     public int enemyLives = 1;
+    public GameObject explosionParticle;
 
 	// Use this for initialization
 	void Start () {
@@ -15,6 +16,8 @@ public class EnemyHealth : MonoBehaviour {
 	void Update () {
 		if(enemyLives <= 0)
         {
+            Instantiate(explosionParticle, transform.position, transform.rotation);
+           
             Destroy(gameObject);
         }
 	}
